e6381607fa9819ce741a9ccea0f02f260d43250e
fermarinsanchez <fermarinsanchez@yahoo.es>
2020-12-04 08:47:17 +0100

    feat: script done. Go for design

---
+++ index.js
@@ -0,0 +1,19 @@
+const playSound = (e) => {
+    console.log(e.keyCode);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    console.log(key)
+    if (!audio) return
+    audio.currentTime = 0
+    audio.play()
+    key.classList.add('playing')
+} 
+
+const removeTransition = (e) => {
+    if (e.propertyName !== 'transform') return
+    this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitioned', removeTransition))
+window.addEventListener('keydown', playSound)
