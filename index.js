1652f4a1af785a67fd4db4ff481082d2a7b4aa92
fermarinsanchez <fermarinsanchez@yahoo.es>
2020-12-06 08:25:49 +0100

    feat: add some style. Still working on elements and samples

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const playSound = (e) => {
-    console.log(e.keyCode);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    console.log(key)
     if (!audio) return
     audio.currentTime = 0
     audio.play()
     key.classList.add('playing')
+    setTimeout(() => { return key.classList.remove('playing')}, 300)
 } 
 
-const removeTransition = (e) => {
-    if (e.propertyName !== 'transform') return
-    this.classList.remove('playing')
+const touchPlay = (e) => {
+    const audio = document.querySelector(`audio[data-key="${e}"]`)
+    const key = document.querySelector(`.key[data-key="${e}"]`)
+    if (!audio) return
+    audio.currentTime = 0
+    audio.play()
+    key.classList.add('playing')
+    setTimeout(() => { return key.classList.remove('playing')}, 300)
 }
 
-const keys = document.querySelectorAll('.key')
-keys.forEach(key => key.addEventListener('transitioned', removeTransition))
+
+
+
+
 window.addEventListener('keydown', playSound)
